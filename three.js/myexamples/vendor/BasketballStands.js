6c329a96162a9da4d7b78f6ae77c67f7ef7c9d3e
YuYeh <a080044976@gmail.com>
2018-09-21 03:24:21 +0800

    add DanboardStoryPlayBasketball_2.0.html

--- three.js/myexamples/vendor/BasketballStands.js
+++ three.js/myexamples/vendor/BasketballStands.js
@@ -17,7 +17,7 @@ class Board  {
 		black 
 		];
 		var material = new THREE.MultiMaterial(boardTexture);
-		var mesh = new THREE.Mesh(new THREE.CubeGeometry(180,105,5),material);
+		var mesh = new THREE.Mesh(new THREE.CubeGeometry(180/200,105/200,5/200),material);
 		this.obj.add (mesh);
 	}
 
@@ -25,7 +25,10 @@ class Board  {
 		this.pc.set (x, y, z)
 		this.obj.position.copy (this.pc);
 	}
-
+	
+	updateWorldPosition(thePos) {
+		this.pc.copy (thePos)
+	}
 }
 class Hoop {
 	constructor(radius=22.5) {
@@ -33,10 +36,10 @@ class Hoop {
 		this.hc = new THREE.Vector3()
 		this.obj = new THREE.Object3D();
 		this.normal = new THREE.Vector3(0,1,0)
-		this.radius = radius/100;
+		this.radius = radius;
 		var material = new THREE.MeshLambertMaterial({
 		map:loader.load( "https://i.imgur.com/uDsEfox.jpg" )});
-		var mesh = new THREE.Mesh(new THREE.TorusGeometry(radius,1,20,100),material);
+		var mesh = new THREE.Mesh(new THREE.TorusGeometry(radius,1/100,20,100),material);
 		this.obj.add(mesh)
 	}
 
@@ -48,5 +51,9 @@ class Hoop {
 	rotateTo (theta) {  // abs CCW angle
 		this.obj.rotation.x = theta;
 		//this.normal.applyEuler (new THREE.Euler (0,0,theta))
+	}
+	
+	updateWorldPosition(thePos) {
+		this.hc.copy (thePos)
 	}
 }
